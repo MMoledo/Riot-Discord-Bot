dc827bf747e0a51bd2ad00f87303cdbd3169a5f3
MMoledo <106261340+MMoledo@users.noreply.github.com>
2023-02-09 21:20:53 -0300

    talvez eu tanha que voltar um dia

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ print("Ligando...")
 id_do_servidor = 871544237458595910
 
 class Client(discord.Client):
+
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.synced = False #Nós usamos isso para o bot não sincronizar os comandos mais de uma vez
@@ -16,16 +17,33 @@ class Client(discord.Client):
         if not self.synced: #Checar se os comandos slash foram sincronizados 
             await tree.sync(guild = discord.Object(id=id_do_servidor)) # Você também pode deixar o id do servidor em branco para aplicar em todos servidores, mas isso fará com que demore de 1~24 horas para funcionar.
             self.synced = True
+
         print(f"Entramos como {self.user}.")
+        channel = client.get_channel(978479957418311700)
+
+        #Confirmacao que esta funcionando
+        print("Liguei!")
+        await channel.send("Liguei!")
+
+        #Loop de Historico
+        Error = True
+        listagem_LOL = await read_LOL()
+        listagem_TFT = await read_TFT()
+
+        while Error:
+            Error = await loop(channel,listagem_TFT,listagem_LOL) 
+            
+        print("Sai Da Execucao, gerando data...")
+        gen_axt()
 
 client = Client()
 tree = app_commands.CommandTree(client)
 
 @tree.command(guild = discord.Object(id=id_do_servidor), name = 'cadastro', description='Cadastre seu nick') #Comando específico para seu servidor
-async def Cadastro(interaction: discord.Interaction):
+async def Cadastro(interaction: discord.Interaction, name: str):    
     view = DropDownView()
-    await interaction.response.send_message('**Selecione o jogo que deseja cadastrar!**',view=view, ephemeral = True) 
-
+    await interaction.response.send_message(f'**Selecione o jogo que deseja cadastrar! {name}**',view=view, ephemeral = True) 
+"""
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -35,7 +53,7 @@ async def on_ready():
     #Confirmacao que esta funcionando
     print("Liguei!")
     await channel.send("Liguei!")
-"""
+
     #Loop de Historico
     Error = True
     listagem_LOL = await read_LOL()
